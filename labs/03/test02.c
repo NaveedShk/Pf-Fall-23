2e5f188d0da0eabae3b4fa61041e2bec6d7be0fa
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-09-05 10:12:56 +0500

    Add files via upload

---
+++ labs/03/test02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+     int len;
+     int brdth;
+      int heig;
+      len=10;
+      brdth=10;
+      heig=20;
+      printf("The volume of the container is %d",len*brdth*heig);
+return 0;
+}
