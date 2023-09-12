b67670a32c436189cd2f7e92115970c9414d80d9
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-09-12 10:11:38 +0500

    Add files via upload

---
+++ labs/04/lab04.c
@@ -0,0 +1,29 @@
+/*  Programmer: Sheikh Naveed Azeemi
+     Roll No: 23K-0003
+    Date: 12-09-2023
+Discription:. Print the pattern for any number of n
+*/
+//--include files--//
+#include<stdio.h>
+#include<math.h>
+int main(){
+int n;
+int i=0;
+char ch='*';
+printf("Enter the value for how much time you want the pattern printed: ");
+scanf("%d",&n);
+while(i<=n)
+
+{ 
+int z=0;
+i++;
+while(z<i)
+{
+z++;
+ printf("*");
+
+}printf("\n");
+}
+return 0;
+
+}//end main
