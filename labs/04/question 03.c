620d5393c07452c5c68fa499b5e98a7828ad53d5
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-09-12 22:16:28 +0500

    Add files via upload

---
+++ labs/04/question 03.c
@@ -0,0 +1,27 @@
+/*
+ * Programmer: Sheikh Naveed Azeemi
+ * Date: 12/9/23
+ * Description: Find sum of squares of n numbers
+ */
+
+//--included libraries--//
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int n, a, num, sum; // amount of inputs, counter for sum, suml, num taken as input
+	sum = 0, a= 0; // initializing sum and counter
+
+	printf("Enter 'n' number of inputs:");
+	scanf("%d", &n);
+
+	while (a < n) {
+		printf("%d. Input number : ", a + 1);
+		scanf("%d", &num);
+		sum += pow(num, 2);
+		a++;
+	}
+
+	printf("sum is: %d", sum);
+
+}// end main
