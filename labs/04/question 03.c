bbccc004dd8749209444d76b2ce1463e9b71b0b2
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-09-12 22:54:14 +0500

    Update question 03.c

--- labs/04/question 03.c
+++ labs/04/question 03.c
@@ -4,24 +4,22 @@
  * Description: Find sum of squares of n numbers
  */
 
-//--included libraries--//
+//--included files--//
 #include <stdio.h>
 #include <math.h>
 
 int main() {
-	int n, a, num, sum; // amount of inputs, counter for sum, suml, num taken as input
-	sum = 0, a= 0; // initializing sum and counter
-
-	printf("Enter 'n' number of inputs:");
-	scanf("%d", &n);
-
-	while (a < n) {
-		printf("%d. Input number : ", a + 1);
-		scanf("%d", &num);
-		sum += pow(num, 2);
-		a++;
+int n, a, num, sum; // amount of inputs, counter for sum, sum, num taken as input
+sum = 0, a= 0; // initializing sum and counter
+printf("Enter 'n' number of inputs:");
+scanf("%d", &n);
+while (a < n) {
+printf("%d. Input number : ", a + 1);
+scanf("%d", &num);
+	sum += pow(num, 2);
+	a++;
 	}
 
-	printf("sum is: %d", sum);
+printf("sum is: %d", sum);
 
 }// end main
