8d1ebaf793cd0592a431b6bfcdd99ab277b59a78
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-09-12 22:53:08 +0500

    Update question 04.c

--- labs/04/question 04.c
+++ labs/04/question 04.c
@@ -9,26 +9,25 @@
 #include <math.h>
 
 int main() {
-	int i, a, b, c; // "i" is the counter and a, b and c for quadratic formula
-	float d; // determinant of equation	
+int i, a, b, c; // "i" is the counter and a, b and c for quadratic formula
+float d; // determinant of equation	
 
-	printf("Input value of 'a': ");
-	scanf("%d", &a);
-	printf("Input value of 'b': ");
-	scanf("%d", &b);
-	printf("Input value of 'c': ");
-	scanf("%d", &c); 
+printf("Input value of 'a': ");
+scanf("%d", &a);
+printf("Input value of 'b': ");
+scanf("%d", &b);
+printf("Input value of 'c': ");
+scanf("%d", &c); 
 
-	// calculating determinant
-	d = pow(b, 2) - (4 * a * c);
-	
-	// identifying if determinant =, < or > 0
-	if (d == 0) {
-		printf("x = %.4f", ( -b + sqrt(d) ) / (2 * a));
-	} else if (d > 0) {
-		printf("x = %.4f and x = %.4f", ( -b + sqrt(d) ) / (2 * a), ( -b - sqrt(d) ) / (2 * a));
-	} else {
-		printf("roots are imaginary");
-	}
+// calculating determinant
+d = pow(b, 2) - (4 * a * c);
+// identifying if determinant =, < or > 0
+if (d == 0) {
+printf("x = %.4f", ( -b + sqrt(d) ) / (2 * a));
+} else if (d > 0) {
+printf("x = %.4f and x = %.4f", ( -b + sqrt(d) ) / (2 * a), ( -b - sqrt(d) ) / (2 * a));
+} else {
+printf("roots are imaginary");
+}
 
 }// end main
