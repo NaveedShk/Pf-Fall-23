90bf2ce76f4815711d9d6c31c0406f692813db60
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-09-19 09:07:04 +0500

    Add files via upload

---
+++ labs/05/question01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+         int num;
+             printf("enter the number:\n");
+             scanf("%d",&num);
+          if(num%3==0)
+          { printf("the entered number is a multiple of 3");
+
+             }//end if 
+           else {
+             printf("the entered number is not a multiple of 3");
+            }//end else
+            }//end main
