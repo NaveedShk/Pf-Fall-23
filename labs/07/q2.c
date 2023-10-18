1fed858d453269b5322ff63d8c31b92c8c41707e
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-10-18 19:21:24 +0500

    Add files via upload

---
+++ labs/07/q2.c
@@ -0,0 +1,47 @@
+
+/*
+ * Programmer: Sheikh Naveed Azeemi
+ * Roll no : 23K-0003
+ * Description: Rotate the elements of an array about a pivot 'd'
+ */
+
+#include <stdio.h>
+
+void showArray(int *arr, int N) {
+    for (int n = 0; n < N; n++) {
+        printf("%d ", *(arr + n));
+    }
+} // end showArray(int *, int)
+
+void rotateElements(int *arr, int N, int p) {
+    for (int k = p - 1, c = 0; k >= 0; k--, c++) {
+        for (int j = k; j + 1 < N - c; j++) { 
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+} // end showArray(int *, int, int)
+
+int main() {
+    int arr[100];
+    int N, d;
+    do {
+        printf("Enter elements: ");
+        scanf("%d", &N);
+    } while (N > 100 || N < 1);
+
+    for (int i = 0; i < N; i++) {
+        printf("Enter element: ");
+        scanf("%d", &arr[i]);
+    } 
+
+    printf("\nEnter pivot: ");
+    scanf("%d", &d);
+
+    rotateElements(arr, N, d);
+    
+    showArray(arr, N);
+
+    return 0;
+} // end main()
