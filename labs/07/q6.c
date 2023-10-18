bb48e1ed4ac76ecf011927bf426d4a78106a4327
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-10-18 19:11:47 +0500

    Add files via upload

---
+++ labs/07/q6.c
@@ -0,0 +1,34 @@
+
+/*
+ * Programmer: Sheikh Naveed Azeemi
+ * Roll no : 23K-0003
+ * Description: check the frequency of number
+ */
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], N, d;
+    int freq[100] = {0};
+    do {
+        printf("Enter elements: ");
+        scanf("%d", &N);
+    } while (N > 100 || N < 1);
+
+    for (int i = 0; i < N; i++) {
+        printf("Enter element: ");
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        freq[arr[i]]++;
+    }
+
+    for (int n = 0; n < 100; n++) {
+        if (!freq[n]) continue;
+        printf("Frequency of %d = %d\n", n, freq[n]);
+    }
+
+
+    return 0;
+} // end main()
