ef54343cc99fb9def38583ed59c389fa2fae87b0
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-11-26 14:44:12 +0500

    Update recursion q4.c

--- labs/09/recursion q4.c
+++ labs/09/recursion q4.c
@@ -1,1 +1,29 @@
+/*
+programmer: Sheikh Naveed Azeemi
+Roll No:23K-0003
+Description: calculating gcd with eucledian algortihm using recursion
+*/
 
+#include <stdio.h>
+
+// Function to find GCD using recursion (Euclidean algorithm)
+int gcd(int a, int b) {
+    if (b == 0) {
+    	//base case
+        return a;
+    }
+    //recursive case
+    return gcd(b, a % b);
+}
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers to find their GCD: ");
+    scanf("%d %d", &num1, &num2);
+    
+    //calling function
+    int result = gcd(num1, num2);
+    printf("GCD using recursion for %d and %d is %d\n", num1, num2, result);
+
+    return 0;
+}
