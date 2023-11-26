694ebceac1271f19567acdd43e6bd73ebcc39b26
NaveedShk <142867585+NaveedShk@users.noreply.github.com>
2023-11-26 14:41:36 +0500

    Create recursion q2.c

---
+++ labs/09/recursion q2.c
@@ -0,0 +1,33 @@
+/*
+Programmer: Sheikh Naveed Azeemi
+Roll No:23K-0003
+Description: multiply 2 numbers using recursion
+*/
+#include<stdio.h> 
+
+int multiply(int a, int b) {
+	if(b==0) {
+		//base case
+	    return 0;
+	} else if(b>0) {
+		//recursive case
+        return a+multiply(a,b-1);
+	} else {
+		//dealing with neg numbers
+	    return -multiply(a, -b);
+	}
+}
+
+main() {
+	int a,b,product;
+	printf("enter num 1:");
+	scanf("%d",&a);
+	printf("enter num 2:");
+	scanf("%d",&b);
+	
+	//calling the function
+	product = multiply(a,b);
+	
+	printf("the product is:%d",product);
+	
+}
